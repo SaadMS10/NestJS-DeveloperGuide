bd8869717afe0ebcb40dbdc62229d3b60d57dfa6
Muhammad Saad <itsmsaadhere@gmail.com>
2021-12-31 18:56:27 +0500

    Creating User

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -10,15 +12,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     UsersModule,
     ReportsModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [],
-    //   synchronize: true,
-    // }),
-    MongooseModule.forRoot(
-      'mongodb+srv://saad:Error1234.@cluster0.wjtwr.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
-    ),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://saad:Error1234.@cluster0.wjtwr.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+    // ),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
+import { Report } from './report.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Report])], //Create Repo For Us
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
 })
 export class ReportsModule {}
