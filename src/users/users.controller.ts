7e8d15697b7734976b71eafc0944bd6b00bcf473
Muhammad Saad <itsmsaadhere@gmail.com>
2022-01-03 15:30:47 +0500

    SignIn,SignUP

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,7 @@ import {
 import { UpdateUserDto } from 'src/dtos/update-user-dto';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 import {
   SerializerInterceptor,
   Serialize,
@@ -22,11 +23,11 @@ import { UserDto } from 'src/dtos/user-dto';
 @Controller('auth')
 @Serialize(UserDto) //custom interceptors and decorators
 export class UsersController {
-  constructor(private usersService: UsersService) {}
-  @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
-  }
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
   @Get('/:id')
   findUser(@Param('id') id: string) {
     const user = this.usersService.findOne(parseInt(id));
@@ -50,5 +51,13 @@ export class UsersController {
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
+  }
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.authService.signup(body.email, body.password);
+  }
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
   }
 }
